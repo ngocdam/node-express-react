214de1f5f91c12b67fe860da97a5ea46c02685cc
ngocdam <ngocdt@cardano-labo.com>
2017-05-03 13:53:22 +0700

    read db from phantomjs

--- src/read-nedb/src/index.js
+++ src/read-nedb/src/index.js
@@ -1,11 +1,12 @@
 import path from 'path';
 import fs from 'fs';
+import db from 'sqlite';
 
 import { readFile, writeFile, copyFile, readDir, makeDir, copyDir, cleanDir } from '../tools/lib/fs';
 
 import startSeq from './sequelize';
 import startNedb from './nedb';
-//import startPhantom from './phantom';
+import startPhantom from './phantom';
 import startWallet from './wallet';
 
 // -----------------------------------------------------------------------------------------
@@ -53,6 +54,31 @@ const getFileName = (filepath) => {
 
 // -----------------------------------------------------------------------------------------
 
+const sqliteTest = async (inputFilePath, outputFilePath) => {
+  // return Promise.resolve()
+  //   // First, try to open the database
+  //   .then(() => db.open(`./${inputFilePath}`, { Promise }))      // <=
+  //   // Update db schema to the latest version using SQL-based migrations
+  //   //.then(() => db.migrate({ force: 'last' }))                  // <=
+  //   // Display error message if something went wrong
+  //   .catch((err) => console.error(err.stack))
+  //   // Finally, launch the Node.js app
+  //   .finally(async () => {
+  //     const records = await db.all('SELECT * FROM Records LIMIT 1');
+  //     console.log('records: ', records);
+  //   });
+  return Promise.resolve().then(() => {
+    var sqlite3 = require('sqlite3').verbose();
+    var db = new sqlite3.Database({
+      filename: `./${inputFilePath}`,
+      mode: sqlite3.OPEN_READWRITE
+    });
+  }).finally(async (db) => {
+    const records = await db.all('SELECT * FROM Records LIMIT 1');
+    console.log('records: ', records);
+  });
+}
+
 const startRead = async (filepath) => {
   //console.log('start read => filepath: ', filepath);
   const ext = '.json';
@@ -63,9 +89,11 @@ const startRead = async (filepath) => {
   const seqOutput = `${outputRoot}/${filename}_SEQUELIZE_${ext}`;
   const walOutput = `${outputRoot}/${filename}_WALLET_${ext}`;
 
-  await startSeq(input, seqOutput); // not much reliability
+  //await startSeq(input, seqOutput); // not much reliability
   //await startNedb(input, nedbOutput); // sometime cannot fully get enough data
-  await startWallet(seqOutput, walOutput);
+  //await startWallet(seqOutput, walOutput);
+  await startPhantom(input, seqOutput);
+  //await sqliteTest(input, seqOutput);
 }
 
 // -----------------------------------------------------------------------------------------
@@ -74,7 +102,7 @@ const startConvert = async (filepath) => {
   await clean();
   console.log('==============================> cleanup done');
 
-  const files = await readDir('**/*.sqlite3', {
+  const files = await readDir(`${inputRoot}/**/*.sqlite3`, {
     nosort: false,
   });
   //console.log('all files: ', files);
@@ -82,9 +110,11 @@ const startConvert = async (filepath) => {
     return path.relative(inputRoot, match);
   });
   //console.log('all file matches: ', matches);
-  matches.map(async (filepath) => {
-    await startRead(filepath);
-  });
+  // matches.map(async (filepath) => {
+  //   await startRead(filepath);
+  // });
+
+  await startRead(matches[0]);
 
 }
 
--- src/read-nedb/src/phantom.js
+++ src/read-nedb/src/phantom.js
@@ -1,4 +1,4 @@
-// yarn add phantom
+import path from 'path';
 import phantom from 'phantom';
 
 import { readFile, writeFile, copyFile, readDir, makeDir, copyDir, cleanDir } from '../tools/lib/fs';
@@ -7,25 +7,196 @@ import { readFile, writeFile, copyFile, readDir, makeDir, copyDir, cleanDir } fr
 // Phantom
 // -----------------------------------------------------------------------------------------
 
-const phantomTest = () => {
-  phantom.create().then(function (ph) {
-    ph.createPage().then(function (page) {
-      page.open('https://stackoverflow.com/').then(function (status) {
-        console.log(status);
-        page.property('content').then(function (content) {
-          console.log(content);
-          page.close();
-          ph.exit();
+// -----------------------------------------------------------------------------------------
+// This function MUST be in pure javascript, because it is executed in browser (phantomjs does not support ES6 in year of 2017)
+function browserEnv(inputFilePath, outputFilePath) {
+  // --------------------------------------------------------------
+  // Javascript environment in this fuction the same as on Browser
+  // --------------------------------------------------------------
+  function log() {
+    console.log('\n\n --------------- Browser console.log - START --------------- \n\n');
+    for (var i = 0; i < arguments.length; i++) {
+      try {
+        console.log(arguments[i] !== null && (typeof arguments[i] === 'object' || arguments[i].constructor === Array) ? JSON.stringify(arguments[i]) : arguments[i]);
+      }
+      catch (ex) {
+        console.log(arguments[i]);
+      }
+      console.log('\n');
+    }
+    console.log('\n --------------- Browser console.log - END --------------- \n\n');
+  }
+  // --------------------------------------------------------------
+  function useNedb() {
+    var db = new window.Nedb({
+      //filename: inputFilePath,
+      filename: 'test_in_browser123',
+      autoload: false,
+      //corruptAlertThreshold: 1
+    });
+    //log(Object.keys(db))
+    db.loadDatabase(function (err) {
+      if (err) {
+        log('load DB ERR')
+      }
+      log('load DB OK')
+      //
+      db.insert({ a: 1, b: 2, timestamp: new Date().getTime() }, function () {
+        db.find({}, function (err, docs) {
+          log('find all err: ', err);
+          log('find all docs: ', docs);
         });
       });
+      //
+      /*db.remove({}, function () {
+        db.insert({ a: 1, b: 2, timestamp: new Date().getTime() }, function () {
+          db.find({}, function (err, docs) {
+            log('find all err: ', err);
+            log('find all docs: ', docs);
+          });
+        });
+      });*/
+      //
     });
+  }
+  // --------------------------------------------------------------
+  function useIndexedDB() {
+    // This works on all devices/browsers, and uses IndexedDBShim as a final fallback 
+    var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+
+    // Open (or create) the database
+    var open = indexedDB.open("MyDatabase", 1);
+
+    // Create the schema
+    open.onupgradeneeded = function () {
+      var db = open.result;
+      var store = db.createObjectStore("MyObjectStore", { keyPath: "id" });
+      var index = store.createIndex("NameIndex", ["name.last", "name.first"]);
+    };
+
+    open.onsuccess = function () {
+      // Start a new transaction
+      var db = open.result;
+      var tx = db.transaction("MyObjectStore", "readwrite");
+      var store = tx.objectStore("MyObjectStore");
+      var index = store.index("NameIndex");
+
+      // Add some data
+      store.put({ id: 12345, name: { first: "John", last: "Doe" }, age: 42 });
+      store.put({ id: 67890, name: { first: "Bob", last: "Smith" }, age: 35 });
+
+      // Query the data
+      var getJohn = store.get(12345);
+      var getBob = index.get(["Smith", "Bob"]);
+
+      getJohn.onsuccess = function () {
+        log(getJohn.result.name.first);  // => "John"
+      };
+
+      getBob.onsuccess = function () {
+        log(getBob.result.name.first);   // => "Bob"
+      };
+
+      // Close the db when the transaction is done
+      tx.oncomplete = function () {
+        db.close();
+      };
+    }
+  }
+  // --------------------------------------------------------------
+  function useLocalforage() {
+    var localforage = window.localforage;
+    localforage.config({
+      driver: localforage.WEBSQL, // Force WebSQL; same as using setDriver()
+      name: 'Localforage_myApp',
+      version: 1.0,
+      size: 4980736, // Size of database, in bytes. WebSQL-only for now.
+      storeName: 'Records', // Should be alphanumeric, with underscores.
+      description: 'Localforage_ some description'
+    });
+    //console.log('localforage.length: ', localforage.length());
+    localforage.getItem('key', function (err, value) {
+      // if err is non-null, we got an error. otherwise, value is the value
+      log(err, value)
+    });
+    // localforage.setItem('Localforage_key', 'Localforage_value', function (err) {
+    //   // if err is non-null, we got an error
+    //   localforage.getItem('Localforage_key', function (err, value) {
+    //     // if err is non-null, we got an error. otherwise, value is the value
+    //   });
+    // });
+  }
+  // --------------------------------------------------------------
+  //useNedb();
+  //useIndexedDB();
+  useLocalforage();
+  // --------------------------------------------------------------
+
+
+}
+// -----------------------------------------------------------------------------------------
+
+const phantomPage = async (inputFilePath, outputFilePath) => {
+
+  console.log('\n' + '===========> phantomPage => start for file: ', inputFilePath);
+
+  const log = console.log;
+  const nolog = () => { };
+
+  const url = 'http://localhost/';
+  // const ph = await phantom.create([], { logLevel: 'error', logger: { warn: log, debug: nolog, error: log } });
+  const ph = await phantom.create();
+  const page = await ph.createPage();
+
+  // check if enable javascript
+  page.setting('javascriptEnabled').then(function (value) {
+    console.log('\n' + 'phantomPage => javascriptEnabled => expect(value).toEqual(true): ', (value === true));
   });
+
+  // config to write console.log from browser to nodejs env console
+  await page.on("onConsoleMessage", msg => process.stdout.write(msg));
+
+  const status = await page.open(url);
+  console.log('\n' + 'phantomPage => page status: ', status);
+
+  const content = await page.property('content');
+  // console.log('phantomPage => page content: ', content);
+
+  // Inject JS files
+  const nm = 'node_modules';
+  const jsList = [
+    'indexeddbshim/dist/indexeddbshim.js',
+    'localforage/dist/localforage.js',
+    'nedb/browser-version/out/nedb.js',
+  ];
+  for (let i = 0; i < jsList.length; i++) {
+    const filename = jsList[i];
+    const filepath = path.join(nm, filename);
+    const isLoaded = await page.injectJs(filepath);
+    console.log('\n' + 'phantomPage => file %s is loaded?: ', filename, isLoaded);
+  }
+  // Inject JS files - END
+
+  // example of excutiion browser javascript
+  const window = await page.evaluate(function (args, get, from, here) {
+    // Javascript environment in this fuction the same as on Browser
+    console.log('Browser => console.log test: OK');
+    return window;
+  }, 'arguments', 'will', 'go', 'here');
+
+  // console.log('phantomPage => window keys: ', Object.keys(window));
+
+  await page.evaluate(browserEnv, inputFilePath, outputFilePath);
+
+  await page.close();
+  await ph.exit();
+
 }
 
 // -----------------------------------------------------------------------------------------
 
-const startPhantom = () => {
-
+const startPhantom = async (inputFilePath, outputFilePath) => {
+  await phantomPage(inputFilePath, outputFilePath);
 }
 
 // -----------------------------------------------------------------------------------------
